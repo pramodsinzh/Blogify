bcd27b556d8b858554093968e8d176b7e66aa49b
Pramodsnz01 <pramodsnz01@gmail.com>
2025-12-15 17:12:02 +0545

    blog and newsletter component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Blog from './pages/Blog'
      <div>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/blog' element={<Blog/>} />
+        <Route path='/blog/:id' element={<Blog/>} />
       </Routes>
      </div>
    )
