109c87df09943ba7b236966e90904cc423661ca6
Pramodsnz01 <pramodsnz01@gmail.com>
2025-12-30 08:42:51 +0545

    Add Blog Page UI complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
- import React from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
@@ -8,23 +8,24 @@ import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
 import Login from './components/admin/Login'
- 
- const App = () => {
-   return (
-     <div>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/blog/:id' element={<Blog/>} />
+import 'quill/dist/quill.snow.css'
 
-        <Route path='/admin' element={true ? <Layout/> : <Login />}>
-          <Route index element={<Dashboard/>} />
-          <Route path='addBlog' element={<AddBlog/>} />
-          <Route path='listBlog' element={<ListBlog/>} />
-          <Route path='comments' element={<Comments/>} />
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<Blog />} />
+
+        <Route path='/admin' element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
         </Route>
       </Routes>
-     </div>
-   )
- }
- 
- export default App
+    </div>
+  )
+}
+
+export default App
