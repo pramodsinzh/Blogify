2d83dd4f8fed5de146546319482c1213c027736c
Pramodsnz01 <pramodsnz01@gmail.com>
2025-12-30 10:32:47 +0545

    Created Basic Express Backend Server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+const app = express()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=> res.send("API is working"))
+
+const PORT = Number(process.env.PORT) || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Access the API at: http://localhost:${PORT}`)
+}) 
+
+export default app; 
