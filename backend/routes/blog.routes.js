bb79086e1254330e350d2f0f2eab04843a92f032
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-02 16:29:19 +0545

    Added multer middleware for image

--- backend/routes/blog.routes.js
+++ backend/routes/blog.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { addBlog } from '../controllers/blog.controller.js';
+import upload from '../middleware/multer.middleware.js';
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog)
+blogRouter.post("/add", upload.single('image'), addBlog)
