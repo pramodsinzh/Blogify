3f0773a077ca35d326d8c42a0d838354d5b4c808
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-03 09:38:02 +0545

    Created controller function for blog getAllBlogs, getBlogById, deleteBlogById, togglePublish

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -42,3 +42,70 @@ export const addBlog = async (req, res) => {
         })
     }
 }
+
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({ isPublished: true })
+        res.json({
+            success: true,
+            message: blogs
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const getBlogById = async (req, res) => {
+    try {
+        const { blogId } = req.parse;
+        const blog = await Blog.findById(blogId)
+        if (!blog) {
+            return res.json({
+                success: false,
+                message: "Blog not found"
+            })
+        }
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export const deleteBlogById = async (req, res) => {
+    try {
+        const { id } = req.body;
+        await Blog.findByIdAndDelete(id)
+        res.json({
+            success: true,
+            message: "Blog deleted successfully!"
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export const togglePublish = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished
+        await blog.save();
+        res.json({
+            success: true,
+            message: "Blog status updated successfully!"
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
