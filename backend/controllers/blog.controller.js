4f938a1522d9dc7d48c33d467013fb73bc3e3373
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-05 16:57:20 +0545

    Created APIs for blog comments

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import imagekit from '../configs/imageKit.config.js';
 import Blog from '../models/blog.model.js';
+import Comment from '../models/comment.model.js';
 
 export const addBlog = async (req, res) => {
     try {
@@ -132,6 +133,38 @@ export const togglePublish = async (req, res) => {
             data: updatedBlog
         })
 
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const addComment = async (req, res) => {
+    try {
+        const {blog, name, content} = req.body;
+        await Comment.create({blog, name, content});
+        res.json({
+            success: true,
+            message: "Comment added for review"
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const getBlogComments = async (req, res) => {
+    try {
+        const {blogId} = req.body;
+        const comments = await Comment.find({ bolg: blogId, isApproved: true}).sort({createdAt: -1});
+        res.json({
+            success: true,
+            message: comments
+        })
     } catch (error) {
         res.json({
             success: false,
--- backend/routes/blog.routes.js
+++ backend/routes/blog.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from '../controllers/blog.controller.js';
+import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComments, togglePublish } from '../controllers/blog.controller.js';
 import upload from '../middleware/multer.middleware.js';
 import auth from '../middleware/auth.middleware.js';
 
@@ -10,5 +10,8 @@ blogRouter.get("/all", getAllBlogs)
 blogRouter.get("/:blogId", getBlogById)
 blogRouter.post("/delete", auth, deleteBlogById)
 blogRouter.post("/toggle-publish", auth, togglePublish)
+
+blogRouter.post('/add-comment', addComment)
+blogRouter.get('/comments', getBlogComments)
 
 export default blogRouter;
