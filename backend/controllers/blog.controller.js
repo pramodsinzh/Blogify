2443fe19a4174431dcc074b41a2a4ec1fedf5322
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-06 17:35:06 +0545

    added admin controller and blog controller functions

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -84,6 +84,10 @@ export const deleteBlogById = async (req, res) => {
     try {
         const { id } = req.body;
         await Blog.findByIdAndDelete(id)
+
+        //Delete comments associaated with this blog
+        await Comment.deleteMany({blog: id})
+        
         res.json({
             success: true,
             message: "Blog deleted successfully!"
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { adminLogin } from '../controllers/admin.controller.js';
+import { adminLogin, approveCommentById, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashboard } from '../controllers/admin.controller.js';
+import auth from '../middleware/auth.middleware.js';
 
 const adminRouter = express.Router();
  
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.get("/delete-comment", auth, deleteCommentById);
+adminRouter.get("/approve-comment", auth, approveCommentById);
+adminRouter.get("/dashboard", auth, getDashboard);
 
 export default adminRouter;
 
