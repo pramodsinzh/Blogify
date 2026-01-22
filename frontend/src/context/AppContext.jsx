2d6feaefaf1df1678c0da78bb918b9eca096bd8c
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-22 16:52:50 +0545

    Added axios and created context for the app

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+
+    const [token, setToken] = useState(null)
+    const [blogs, setBlogs] = useState([])
+    const [input, setInput] = useState("")
+
+    const value = { axios, navigate, token, setToken, blogs, setBlogs, input, setInput }
+    
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
