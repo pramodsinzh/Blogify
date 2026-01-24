6330dfabdc37a2906f21b9faf8fc274ded8ba262
Pramodsnz01 <pramodsnz01@gmail.com>
2026-01-24 09:07:35 +0545

    Fix blog page - use useAppContext hook correctly and update comments API call to use query params

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -6,7 +6,7 @@ const BlogCard = ({ blog }) => {
     const navigate = useNavigate()
     return (
         <>
-            <div onClick={()=> navigate(`/blog/${_id}`)} className="w-full rounded-lg overflow-hidden hover:scale-102 hover:shadow-primary/25 transition duration-300 cursor-pointer">
+            <div onClick={()=> navigate(`/blog/${_id}`)} className="w-full rounded-lg overflow-hidden shadow-md hover:scale-102 hover:shadow-primary/25 transition duration-300 cursor-pointer">
                 <img src={image} className='aspect-video' alt="" />
                 <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs'>{category}</span>
                 <div className="p-5">
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -5,34 +5,62 @@ import Navbar from '../components/Navbar'
 import Moment from 'moment'
 import Footer from '../components/Footer'
 import { Loader } from '../components/Loader'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Blog = () => {
   const { id } = useParams()
 
+  const {axios} = useAppContext()
+
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([])
-
   const [name, setName] = useState('')
   const [content, setContent] = useState('')
 
   const fetchBlogData = async () => {
-    const data = blog_data.find(item => item._id === id)
-    setData(data)
+    try {
+       const {data} = await axios.get(`/blog/${id}`)
+       data.success ? setData(data.blog) : toast.error(data.message)
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   const fetchComments = async () => {
-    setComments(comments_data)
+    try {
+      const {data} = await axios.get(`/blog/comments?blogId=${id}`)
+      if(data.success){
+        setComments(data.comments)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   const addComment = async (e) => {
     e.preventDefault();
+    try {
+      const {data} = await axios.post('/blog/add-comment', {blog: id, name, content})
+      if(data.success){
+        toast.success(data.message)
+        setName('')
+        setContent('')
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
 
   useEffect(() => {
     fetchBlogData()
     fetchComments()
-  }, [])
+  }, [id])
   return data ? (
     <div className='relative'>
       <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-50' alt="" />
