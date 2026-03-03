45a28255dda80a584764345e0db6b9f0c2463f00
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-03 08:47:13 +0545

    UI correction

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -97,7 +97,7 @@ const Blog = () => {
       <div className="text-center mt-20 text-gray-600">
         <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
         <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
-        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <h2 className='my-5 max-w-lg mx-auto'>{data.subTitle}</h2>
         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>
           {data.category}
         </p>
